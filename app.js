227eb9abfb6f120a3c9c6ab40efe739af589c061
alexthegreat194 <alexander.gentry@aol.com>
2022-07-10 13:41:04 -0700

    added logging

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 
 require('dotenv').config();
 
@@ -10,6 +11,10 @@ app.use(cors({
     origin: '*'
 }));
 app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+// custom middlware
 
 const auth = require('./routes/auth.js');
 const profile = require('./routes/profile.js');
