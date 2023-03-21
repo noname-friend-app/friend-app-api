79c28227f1863ee3cf3b3eed98b88f5f0cd4f50c
alexthegreat194 <alexander.gentry@aol.com>
2023-03-20 19:34:46 -0700

    elias changes

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: '*'
+    origin: ['http://localhost:3000'],
+    credentials: true
 }));
 app.use(bodyParser.json());
 app.use(session({
